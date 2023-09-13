f195d3a5cf0e376a04baa34c5ecf2459011d343d
Matt Davies <mattwdavies@Matts-MacBook-Air.local>
2023-09-13 21:33:30 +0100

    build: initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import styles from './styles.module.scss'
+export default function Home() {
+  return (
+    <main >
+      <p className={styles.boo}>
+        BOOOOOOO
+      </p>
+    </main>
+  )
+} 
